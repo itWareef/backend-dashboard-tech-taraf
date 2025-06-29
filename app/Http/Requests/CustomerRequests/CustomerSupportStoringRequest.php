0059ba5e984a7529f18dc46f546ed9dcdeb6dfb0
usman ahmed fathy <usmanahmedfathy@gmail.com>
2025-06-29 16:46:24 +0300

    update

--- app/Http/Requests/CustomerRequests/CustomerSupportStoringRequest.php
+++ app/Http/Requests/CustomerRequests/CustomerSupportStoringRequest.php
@@ -24,7 +24,7 @@ class CustomerSupportStoringRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'note'      =>['required','sting' ,],
+            'note'      =>['required','string' ,],
 
         ];
     }
