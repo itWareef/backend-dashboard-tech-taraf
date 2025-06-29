707b6170f9b3b6d066e777b8878116de086c077a
usman ahmed fathy <usmanahmedfathy@gmail.com>
2025-06-29 12:23:33 +0300

    update admin auth

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,59 +21,67 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        $owner = Customer::create([
-            'name' => 'Usman Ahmed',
-            'first_name' => 'Usman',
-            'last_name' => 'Ahmed',
-            'email' => 'usmanahmedfathy@gmail.com',
-            'password' => Hash::make('123456789'),
-        ]);
-        $developer = Developer::create([
-            'name' => 'Wareef'
-        ]);
-        $project = Project::create([
-            'name'  => 'project 40',
-            'developer_id' => $developer->id,
-            'count' => 80,
-            'place' => 'ksa'
-        ]);
-        $unit = Unit::create([
-            'owner_id' => $owner->id,
-            'project_id' => $project->id,
-            'villa_number'  => '852262',
-            'deed_number'   => '855123',
-            'purchase_date' => '2025-06-26',
-            'number' => '215165'
-        ]);
-
-        Contract::create([
-            'unit_id' => $unit->id,
-            'customer_id' => $owner->id,
-            'start_date' => '2025-06-26',
-            'end_date'=> '2025-06-30',
-            'type' => 'maintenance'
-        ]);
-        Contract::create([
-            'unit_id' => $unit->id,
-            'customer_id' => $owner->id,
-            'start_date' => '2025-06-26',
-            'end_date'=> '2025-06-30',
-            'type' => 'planting'
-        ]);
-        Supervisor::create([
+//        $owner = Customer::create([
+//            'name' => 'Usman Ahmed',
+//            'first_name' => 'Usman',
+//            'last_name' => 'Ahmed',
+//            'email' => 'usmanahmedfathy@gmail.com',
+//            'password' => Hash::make('123456789'),
+//        ]);
+//        $developer = Developer::create([
+//            'name' => 'Wareef'
+//        ]);
+//        $project = Project::create([
+//            'name'  => 'project 40',
+//            'developer_id' => $developer->id,
+//            'count' => 80,
+//            'place' => 'ksa'
+//        ]);
+//        $unit = Unit::create([
+//            'owner_id' => $owner->id,
+//            'project_id' => $project->id,
+//            'villa_number'  => '852262',
+//            'deed_number'   => '855123',
+//            'purchase_date' => '2025-06-26',
+//            'number' => '215165'
+//        ]);
+//
+//        Contract::create([
+//            'unit_id' => $unit->id,
+//            'customer_id' => $owner->id,
+//            'start_date' => '2025-06-26',
+//            'end_date'=> '2025-06-30',
+//            'type' => 'maintenance'
+//        ]);
+//        Contract::create([
+//            'unit_id' => $unit->id,
+//            'customer_id' => $owner->id,
+//            'start_date' => '2025-06-26',
+//            'end_date'=> '2025-06-30',
+//            'type' => 'planting'
+//        ]);
+//        Supervisor::create([
+//            'name' => 'Test User',
+//            'first_name' => 'Test',
+//            'last_name' => 'User',
+//            'type' => 'planting',
+//            'email' => 'test@example.com',
+//            'password' => Hash::make('123456789'),
+//        ]);
+//        Supervisor::create([
+//            'name' => 'Test User2',
+//            'first_name' => 'Test',
+//            'last_name' => 'User2',
+//            'type' => 'maintenance',
+//            'email' => 'test2@example.com',
+//            'password' => Hash::make('123456789'),
+//        ]);
+        User::create([
             'name' => 'Test User',
             'first_name' => 'Test',
             'last_name' => 'User',
-            'type' => 'planting',
-            'email' => 'test@example.com',
-            'password' => Hash::make('123456789'),
-        ]);
-        Supervisor::create([
-            'name' => 'Test User2',
-            'first_name' => 'Test',
-            'last_name' => 'User2',
-            'type' => 'maintenance',
-            'email' => 'test2@example.com',
+            'role' => 'admin',
+            'email' => 'usmanahmedfathy@gmail.com',
             'password' => Hash::make('123456789'),
         ]);
     }
