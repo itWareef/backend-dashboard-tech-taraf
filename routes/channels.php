cc8f5550bd23044dff97d3478e672671a78b86e5
usman ahmed fathy <usmanahmedfathy@gmail.com>
2025-07-03 07:12:00 +0300

    update

--- routes/channels.php
+++ routes/channels.php
@@ -6,6 +6,3 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
-Broadcast::channel('gps-tracking', function ($user) {
-    return Auth::guard('api')->check();
-});
