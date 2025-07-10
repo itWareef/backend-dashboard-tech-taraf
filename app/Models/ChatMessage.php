1aaf1002dfca17ff44171c6c2586ee6e70035cdd
usman ahmed fathy <usmanahmedfathy@gmail.com>
2025-07-10 12:45:53 +0300

    update

--- app/Models/ChatMessage.php
+++ app/Models/ChatMessage.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChatMessage extends Model
 {
-    //
+    protected $fillable=[
+        'user_id',
+        'message' ,
+        'sender',
+    ];
 }
