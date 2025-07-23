3edb365ccc6ad9d7457f2f24eb6905e38d031cdf
usman ahmed fathy <usmanahmedfathy@gmail.com>
2025-07-23 21:34:57 +0300

    update

--- app/Models/Project/Project.php
+++ app/Models/Project/Project.php
@@ -2,10 +2,12 @@
 
 namespace App\Models\Project;
 
+use App\Core\Interfaces\HasManyRelations;
 use App\Models\Developer;
+use App\Models\Guarantee;
 use Illuminate\Database\Eloquent\Model;
 
-class Project extends Model
+class Project extends Model implements HasManyRelations
 {
     protected $fillable=[
         'name',
@@ -18,5 +20,16 @@ class Project extends Model
     }
     public function developer(){
         return $this->belongsTo(Developer::class,'developer_id');
+    }
+    public function guarantees()
+    {
+        return $this->belongsToMany(Guarantee::class, 'gurantee_project');
+    }
+
+    public function getHasManyRelations(): array
+    {
+        return [
+            'guarantees'
+        ];
     }
 }
