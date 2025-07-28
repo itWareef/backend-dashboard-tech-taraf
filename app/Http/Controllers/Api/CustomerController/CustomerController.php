498c5a99634b1d771ea323bd214d8778067e9373
usman ahmed fathy <usmanahmedfathy@gmail.com>
2025-07-28 10:34:39 +0300

    test

--- app/Http/Controllers/Api/CustomerController/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController/CustomerController.php
@@ -163,7 +163,7 @@ class CustomerController extends Controller
     }
     public function myRequests(){
             $userId = auth('customer')->user()->id;
-            $orders = QueryBuilder::for(Order::class)->with('items')->where('customer_id' , $userId)->get();
+            $orders = QueryBuilder::for(Order::class)->with('coupon','items.brand')->where('customer_id' , $userId)->get();
             $requestService = QueryBuilder::for(ServiceRequest::class)->where('requester_id',$userId)->get();
             $requestContract = QueryBuilder::for(ContractRequest::class)->where('customer_id',$userId)->get();
             $requestPlanting = QueryBuilder::for(GardenRequest::class)->with('attachments')->where('customer_id',$userId)->get();
